6416b03fe8272bc3de97cab3ac609ee79696aea5
Otieno Collins Junior <141353498+CollinsAnyona@users.noreply.github.com>
2024-06-07 00:25:03 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
-# web static development
+# describe it then
+apt-get update
+apt-get -y install nginx
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Hello, this is a test HTML file." | sudo tee /data/web_static/releases/test/index.html
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-sudo service nginx restart
+directories=("/data/web_static/releases/test" "/data/web_static/shared/")
+
+for directory in "${directories[@]}"; do
+  #  if [ ! -e "$directory" ]; then
+  mkdir -p "$directory"
+  #  fi
+done
+
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" >/data/web_static/releases/test/index.html
+
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
+# If the symbolic link already exists, it should be deleted and recreated every time the script is ran
+ln --symbolic --force /data/web_static/releases/test /data/web_static/current
+
+# The -R option ensures that the ownership changes are applied
+# recursively to all files and directories within the folder
+chown -R ubuntu:ubuntu /data/
+
+sed -i '/listen 80 default_server;/a \ \n    location /hbnb_static {\n        alias /data/web_static/current/;\n        index index.html;\n    }' /etc/nginx/sites-available/default
+
+service nginx restart
