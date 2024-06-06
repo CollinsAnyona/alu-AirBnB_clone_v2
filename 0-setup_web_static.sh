80f849cfb58dca231475970632735defea0e16a2
Otieno Collins Junior <141353498+CollinsAnyona@users.noreply.github.com>
2024-06-07 00:36:09 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# This script sets up your web servers for the deployment of web_static
+
+# Exit on error
+set -e
 
 # Install Nginx if it is not already installed
 if ! dpkg -l | grep -q nginx; then
@@ -6,7 +10,7 @@ if ! dpkg -l | grep -q nginx; then
     apt-get -y install nginx
 fi
 
-# Create the necessary directories
+# Create the necessary directories with appropriate permissions
 mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # Create a fake HTML file to test the Nginx configuration
@@ -30,8 +34,8 @@ chown -R ubuntu:ubuntu /data/
 # Update the Nginx configuration
 nginx_config="/etc/nginx/sites-available/default"
 
-if ! grep -q "location /hbnb_static/" $nginx_config; then
-    sed -i "/^\s*server_name _;/a \\\n    location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }\n" $nginx_config
+if ! grep -q "location /hbnb_static/" "$nginx_config"; then
+    sed -i "/^\s*server_name _;/a \\\n    location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }\n" "$nginx_config"
 fi
 
 # Restart Nginx to apply the changes
