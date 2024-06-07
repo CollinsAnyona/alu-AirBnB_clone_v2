a748643efebc3ea6dc0ba9c259ce74505ba86616
CollinsAnyona <collinsanyona66@gmail.com>
2024-06-07 11:37:32 +0300

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,47 +1,32 @@
 #!/usr/bin/env bash
-# This script sets up your web servers for the deployment of web_static
+# describe it then
+apt-get update
+apt-get -y install nginx
 
-# Exit on error
-set -e
+directories=("/data/web_static/releases/test" "/data/web_static/shared/")
 
-# Install Nginx if it is not already installed
-if ! dpkg -l | grep -q nginx; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-    sudo ufw allow 'Nginx HTTP'
-fi
+for directory in "${directories[@]}"; do
+  #  if [ ! -e "$directory" ]; then
+  mkdir -p "$directory"
+  #  fi
+done
 
-# Create the necessary directories with appropriate permissions
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-# Create a fake HTML file to test the Nginx configuration
 echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" >/data/web_static/releases/test/index.html
 
-# Create (or recreate) the symbolic link
-if [ -L /data/web_static/current ]; then
-    sudo rm /data/web_static/current
-fi
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
+# If the symbolic link already exists, it should be deleted and recreated every time the script is ran
+ln --symbolic --force /data/web_static/releases/test /data/web_static/current
 
-# Give ownership of the /data/ folder to the ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
+# The -R option ensures that the ownership changes are applied
+# recursively to all files and directories within the folder
+chown -R ubuntu:ubuntu /data/
 
-# Update the Nginx configuration
-nginx_config="/etc/nginx/sites-available/default"
+sed -i '/listen 80 default_server;/a \ \n    location /hbnb_static {\n        alias /data/web_static/current/;\n        index index.html;\n    }' /etc/nginx/sites-available/default
 
-if ! grep -q "location /hbnb_static/" "$nginx_config"; then
-    sudo sed -i "/^\s*server_name _;/a \\\n    location /hbnb_static/ {\n        alias /data/web_static/current/;\n        autoindex off;\n    }\n" "$nginx_config"
-fi
-
-# Restart Nginx to apply the changes
-sudo service nginx restart
-
-# Ensure the script exits successfully
-exit 0
-
+service nginx restart
