d36d64059a73b0d58bd012c939ec8dbeb7128098
CollinsAnyona <collinsanyona66@gmail.com>
2024-06-07 11:31:45 +0300

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,13 +6,13 @@ set -e
 
 # Install Nginx if it is not already installed
 if ! dpkg -l | grep -q nginx; then
-    apt-get update
-    apt-get -y install nginx
-    ufw allow 'Nginx HTTP'
+    sudo apt-get update
+    sudo apt-get -y install nginx
+    sudo ufw allow 'Nginx HTTP'
 fi
 
 # Create the necessary directories with appropriate permissions
-mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # Create a fake HTML file to test the Nginx configuration
 echo "<html>
@@ -21,29 +21,26 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Create (or recreate) the symbolic link
 if [ -L /data/web_static/current ]; then
-    rm /data/web_static/current
+    sudo rm /data/web_static/current
 fi
-ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user and group
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration
 nginx_config="/etc/nginx/sites-available/default"
 
 if ! grep -q "location /hbnb_static/" "$nginx_config"; then
-    sed -i "/^\s*server_name _;/a \\\n    location /hbnb_static/ {\n        alias /data/web_static/current/;\n        autoindex off;\n    }\n" "$nginx_config"
+    sudo sed -i "/^\s*server_name _;/a \\\n    location /hbnb_static/ {\n        alias /data/web_static/current/;\n        autoindex off;\n    }\n" "$nginx_config"
 fi
 
 # Restart Nginx to apply the changes
-service nginx restart
-
-# Check the status of the nginx service
-service nginx status
+sudo service nginx restart
 
 # Ensure the script exits successfully
 exit 0
