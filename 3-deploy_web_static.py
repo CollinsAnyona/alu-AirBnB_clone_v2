75db60f9b6305a12b8420f771fbed430da9f48c5
CollinsAnyona <collinsanyona66@gmail.com>
2024-06-07 23:24:56 +0300

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -44,5 +44,38 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
 
         # Create the release directory
-        run
+        run("mkdir -p {}{}/".format(path, no_ext))
+
+        # Uncompress the archive to the release directory
+        run("tar -xzf /tmp/{} -C {}{}/".format(filename, path, no_ext))
+
+        # Delete the archive from the web server
+        run("rm /tmp/{}".format(filename))
+
+        # Move contents to the proper directory
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, no_ext))
+
+        # Remove the extraneous directory
+        run("rm -rf {}{}/web_static".format(path, no_ext))
+
+        # Delete the current symbolic link
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link
+        run("ln -s {}{}/ /data/web_static/current".format(path, no_ext))
+
+        return True
+    except Exception as e:
+        return False
+
+def deploy():
+    """
+    Creates and distributes an archive to web servers.
+    Returns:
+        bool: True if all operations have been done correctly, otherwise False.
+    """
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
 
